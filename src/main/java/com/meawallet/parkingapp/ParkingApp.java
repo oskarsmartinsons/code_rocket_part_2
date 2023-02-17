bc189970dc603e7ceea848c4dabce159b1f56705
oskarsmartinsons <oskarsmartinsons2016@gmail.com>
2023-02-17 12:51:19 +0200

    New Project

---
+++ src/main/java/com/meawallet/parkingapp/ParkingApp.java
@@ -0,0 +1,8 @@
+package com.meawallet.parkingapp;
+
+public class ParkingApp {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/test/java/com/meawallet/parkingapp/ParkingAppTests.java
@@ -0,0 +1,11 @@
+package com.meawallet.parkingapp;
+
+import org.junit.jupiter.api.Test;
+
+class ParkingAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
